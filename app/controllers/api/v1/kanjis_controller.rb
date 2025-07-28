30fcb9384a5b453ad7e4da0a37027e19a7cfaf82
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-28 11:30:31 -0400

    Kanjis Controller Index created

--- app/controllers/api/v1/kanjis_controller.rb
+++ app/controllers/api/v1/kanjis_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::KanjisController < ApplicationController
+  def index
+    @kanjis = Kanji.all
+    if params[:character].present?
+      @kanjis = Kanji.where('character ILIKE ?', "%#{params[:character]}%")
+    else
+      @kanjis = Kanji.all
+    end
+
+    render json: @kanjis
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # as a user, i can see all of my kanji
   # /api/v1/kanjis
   # get '/api/v1/cafes'
-  # namespace :api, defaults {format: :jason} do
-  #  namespace :v1 do
-  #   resources :kanjis, only: [ :index]
-  #  end
-  # end
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :kanjis, only: [ :index, :show ]
+    end
+  end
 
   # as a user, can search through all of my kanji
   # as a user, i can add a kanji
